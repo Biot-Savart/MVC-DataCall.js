20ba4ad1477339a263d89ea728a2e6b91c518fdf
Ryno Myburgh <rynomyburgh@gmail.com>
2016-11-17 14:30:11 +0200

    Add files via upload

---
+++ MVC-DataCall.js
@@ -0,0 +1,78 @@
+﻿//Created by Ryno Myburgh (Biot-Savart)
+
+var MVC_DataCall =
+    {
+        dfd: jQuery.Deferred(),
+        getJsonPost: function (controller, method, jsonObjectString) {
+            var dfd = jQuery.Deferred();
+
+            $.ajax({
+                url: '../' + controller + '/' + method,
+                data: jsonObjectString,
+                contentType: 'application/json',
+                dataType: 'json',
+                type: 'POSt',
+                success: function (data) {                    
+                    dfd.resolve(data);                   
+                },
+                error: function (err) {
+                    dfd.reject(err);
+                }
+            });
+
+            return dfd.promise();
+        },
+        getJson: function (controller, method, jsonObjectString) {
+            var dfd = jQuery.Deferred();
+
+            $.ajax({
+                url: '../' + controller + '/' + method,
+                data: jsonObjectString,
+                contentType: 'application/json',
+                dataType: 'json',               
+                success: function (data) {
+                    dfd.resolve(data);
+                },
+                error: function (err) {
+                    dfd.reject(err);
+                }
+            });
+
+            return dfd.promise();
+        },
+        getViewPost: function (controller, method, jsonObjectString) {
+            var dfd = jQuery.Deferred();
+
+            $.ajax({
+                url: '../' + controller + '/' + method,
+                data: jsonObjectString,
+                contentType: 'application/json',
+                type: 'POSt',
+                success: function (data) {
+                    dfd.resolve(data);
+                },
+                error: function (err) {
+                    dfd.reject(err);
+                }
+            });
+
+            return dfd.promise();
+        },
+        getView: function (controller, method, jsonObjectString) {
+            var dfd = jQuery.Deferred();
+
+            $.ajax({
+                url: '../' + controller + '/' + method,
+                data: jsonObjectString,
+                contentType: 'application/json',               
+                success: function (data) {
+                    dfd.resolve(data);
+                },
+                error: function (err) {
+                    dfd.reject(err);
+                }
+            });
+
+            return dfd.promise();
+        }
+    }
